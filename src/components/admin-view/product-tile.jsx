30809546efa1fdaecd19ff75654f1ef4f61cd039
Mostafa Alagmy <mostafa@example.com>
2025-01-12 14:04:44 +0200

    fivth

--- src/components/admin-view/product-tile.jsx
+++ src/components/admin-view/product-tile.jsx
@@ -1,5 +1,5 @@
 import { Button } from "../ui/button";
-import { Card, CardContent, CardFooter } from "@/ui/card";
+import { Card, CardContent, CardFooter } from "../ui/card";
 
 function AdminProductTile({
   product,
